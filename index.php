5d6667de632cbc5ecec0f89a0dc1002f2fac17fe
Peter Hajas <peter@haj.as>
2022-05-15 22:42:43 -0400

    write date text file

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 $input = file_get_contents('php://input');
 if (strlen($input) > 0) {
     $result = file_put_contents('test.json', $input);
+    file_put_contents('date.txt', date(DATE_RFC2822));
     echo('OK');
 }
 else {
